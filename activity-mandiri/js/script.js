8e2874a3f50a3eaf91ec9601023a3d45a9dc81c3
Christian Farrel <farrelyoga2000@gmail.com>
2025-11-13 15:00:46 +0700

    Add files via upload

---
+++ activity-mandiri/js/script.js
@@ -0,0 +1,35 @@
+function fn_ValForm() {
+    var sMsg = "";
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    
+    if (name == "") {
+        sMsg += "\n- Anda belum mengisikan nama";
+    }
+
+    
+    if (email == "") {
+        sMsg += "\n- Anda belum mengisikan email";
+    } else {
+        
+        var emailPattern = /^[a-z0-9][a-z0-9_\.\-]{0,}[a-z0-9]@[a-z0-9][a-z0-9_\.\-]{0,}[a-z0-9]\.[a-z0-9]{2,4}$/i;
+        if (!emailPattern.test(email)) {
+            sMsg += "\n- Format email tidak valid (contoh: nama@email.com)";
+        }
+    }
+
+    if (message == "") {
+        sMsg += "\n- Anda belum mengisikan pesan";
+    }
+
+  
+    if (sMsg != "") {
+        alert("Peringatan:" + sMsg);
+        return false;
+    } else {
+        alert("Form berhasil dikirim!");
+        return true;
+    }
+}
